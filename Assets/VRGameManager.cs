18fc91ebd408346c785218d2e6e3fb95eee070dc
Jailson S <jailson.silva@ngx.bet>
2024-10-22 17:41:28 -0300

    Versao dia 22

--- Assets/VRGameManager.cs
+++ Assets/VRGameManager.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VRGameManager : MonoBehaviour
 {
     public int score = 0; // Inicializa o score com 0
     public int victoryScore = 0;
 
+    public TextMeshProUGUI gameoverOrganicText;
+    public TextMeshProUGUI gameoverGlassText;
+    public TextMeshProUGUI gameoverPaperText;
+    public TextMeshProUGUI gameoverPlasticText;
     public TextMeshProUGUI organicText;
     public TextMeshProUGUI glassText;
     public TextMeshProUGUI paperText;
@@ -22,15 +27,15 @@ public class VRGameManager : MonoBehaviour
 
     public TextMeshProUGUI timerText; // Texto para exibir o tempo
     private float timer = 0f; // Variável que armazenará o tempo decorrido
-    public float gameDuration = 120f; // Tempo limite do jogo (2 minutos)
+    public float gameDuration = 240f; // Tempo limite do jogo (4 minutos)
 
     public int totalTrashCount; // Contador total de lixo coletado
     public TextMeshProUGUI victoryScoreText;
     void Start()
     {
         timer = gameDuration; // Inicializa o timer com o tempo total de jogo
-        Victory.SetActive(false); // Certifica-se de que Victory não está ativo no início
-        GameOver.SetActive(false); // Certifica-se de que GameOver não está ativo no início
+        //Victory.SetActive(false); // Certifica-se de que Victory não está ativo no início
+        //GameOver.SetActive(false); // Certifica-se de que GameOver não está ativo no início
         UpdateTimerText(); // Atualiza o texto do timer no início
     }
 
@@ -63,21 +68,25 @@ public class VRGameManager : MonoBehaviour
         {
             organicCount++;
             organicText.text = organicCount.ToString() + "/4";
+            gameoverOrganicText.text = organicCount.ToString() + "/4";
         }
         else if (tag == "LixoVidro")
         {
             glassCount++;
             glassText.text = glassCount.ToString() + "/4";
+            gameoverGlassText.text = glassCount.ToString() + "/4";
         }
         else if (tag == "LixoPapel")
         {
             paperCount++;
             paperText.text = paperCount.ToString() + "/4";
+            gameoverPaperText.text = paperCount.ToString() + "/4";
         }
         else if (tag == "LixoPlastico")
         {
             plasticCount++;
             plasticText.text = plasticCount.ToString() + "/4";
+            gameoverPlasticText.text = plasticCount.ToString() + "/4";
         }
     }
 
@@ -89,5 +98,14 @@ public class VRGameManager : MonoBehaviour
 
         // Atualiza o texto do Timer no formato MM:SS
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+    
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
